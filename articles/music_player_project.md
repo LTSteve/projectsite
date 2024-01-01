b739f58dd732751456c17f948aaa11d08ce47c0b
LTSteve <Steth1000@gmail.com>
2023-12-31 22:05:33 -0500

    music player

--- articles/music_player_project.md
+++ articles/music_player_project.md
@@ -1,2 +1,6 @@
 Some music should start playing on this page when you move the slider... if there is no slider, it's because I haven't actually worked on this at all...
-<div data-subtle-music-player="/music/shouldnthaveletmyselfwant.wav"></div>
+<div data-subtle-music-player="/music/shouldnthaveletmyselfwant.wav">
+<div style="visibility: hidden;position:absolute;">
+<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1616334309&color=%23ff5500&auto_play=false&hide_related=true&show_comments=false&show_user=false&show_reposts=false&show_teaser=false&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/user-456979416" title="|]um¶sterƒiræday" target="_blank" style="color: #cccccc; text-decoration: none;">|]um¶sterƒiræday</a> · <a href="https://soundcloud.com/user-456979416/uss-ps21026" title="USS PS21026" target="_blank" style="color: #cccccc; text-decoration: none;">USS PS21026</a></div>
+</div>
+</div>
--- scripts/elements/subtleMusicPlayer.js
+++ scripts/elements/subtleMusicPlayer.js
@@ -1,7 +1,10 @@
 import styles from "/css/subtleMusicPlayer.css" assert {type: 'css'}
+import { SC } from "/scripts/lib/sc.js"
 document.adoptedStyleSheets = [styles]
 
 const _subtleMusicPlayer = (()=>{
+    let currentInterval = null;
+    
     let apply = (rootElement)=>{
         var toApplyTo = rootElement.querySelectorAll('[data-subtle-music-player]');
         if(!toApplyTo || toApplyTo.length === 0) return;
@@ -13,6 +16,25 @@ const _subtleMusicPlayer = (()=>{
             input.max = 100;
             input.value = 0;
             element.appendChild(input);
+            
+            let scWidget = SC.Widget(element.querySelector("iframe"));
+            input.addEventListener("mousedown",(ev)=>{
+                if(currentInterval){
+                    window.clearInterval(currentInterval);
+                    currentInterval = null;
+                }
+                scWidget.play();
+                currentInterval = window.setInterval(()=>{
+                    scWidget.setVolume(input.value);
+                }, 80);
+            });
+            input.addEventListener("mouseup",(ev)=>{
+                window.clearInterval(currentInterval);
+                currentInterval = null;
+                if(input && input.value === "0"){
+                    scWidget.pause();
+                }
+            });
         }
     }
     return {
